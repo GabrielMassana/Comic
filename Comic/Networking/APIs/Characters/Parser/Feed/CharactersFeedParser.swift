d9b2ef1ebd623362d82f899926cb93490b495e5c
Gabriel Massana <gabrielmassana@gmail.com>
2016-03-19 13:12:56 +0000

    added feed parser class

---
+++ Comic/Networking/APIs/Characters/Parser/Feed/CharactersFeedParser.swift
@@ -0,0 +1,15 @@
+//
+//  CharactersFeedParser.swift
+//  Comic
+//
+//  Created by Gabriel Massana on 19/3/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+import UIKit
+
+import CoreDataFullStack
+
+class CharactersFeedParser: Parser {
+
+}
